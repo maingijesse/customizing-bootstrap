0a0d7ea7f55998548ff23323c505e81f9246c27e
CJ <maingijesse@gmail.com>
2019-07-26 13:12:50 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,77 @@
+$(function(){
+
+	$(".typed").typed({
+		strings: ["Hello there.", "My Name is Jesse.", "I am a Web Designer.", "Web Developer","From Nairobi, Kenya"],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 40,
+		// time before typing starts
+		startDelay: 500,
+		// backspacing speed
+		backSpeed: 30,
+		// time before backspacing 
+		backDelay: 700,
+		// loop
+		loop: true,
+		// false = infinite
+		// show cursor
+		showCursor: true,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'text',
+		// call when done callback function
+		// callback: function() {},
+		// // starting callback function before each string
+		// preStringTyped: function() {},
+		// //callback for every typed string
+		// onStringTyped: function() {},
+		// // callback for reset
+		// resetCallback: function() {}
+	});
+
+
+	
+});
+
+
+// implimenting smooth scroll
+
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
